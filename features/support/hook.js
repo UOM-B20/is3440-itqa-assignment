e3697f2d307f249a2bff950040209fb7e5c6f7dc
kingmalitha <sandaruwanmalitha2018@gmail.com>
2025-01-02 14:04:46 +0530

    finalized ui

--- features/support/hook.js
+++ features/support/hook.js
@@ -6,22 +6,36 @@ const {
   AfterAll,
 } = require("@cucumber/cucumber");
 const serverUtils = require("./server-utils");
+const uiUtils = require("./ui-utils");
 
 setDefaultTimeout(60 * 1000);
 
-// BeforeAll({ tags: "@api" }, async function () {
-//   await serverUtils.startServer();
-// });
+BeforeAll({ tags: "@api" }, async function () {
+  await serverUtils.startServer();
+});
 
-// AfterAll({ tags: "@api" }, async function () {
-//   await serverUtils.shutdown();
-// });
+AfterAll({ tags: "@api" }, async function () {
+  await serverUtils.shutdown();
+});
+
+BeforeAll({ tags: "@ui" }, async function () {
+  await uiUtils.initBrowser();
+});
+
+AfterAll({ tags: "@ui" }, async function () {
+  await uiUtils.closeBrowser();
+});
 
 Before(async function ({ pickle }) {
   // UI test setup
   if (pickle.tags.some((tag) => tag.name === "@ui")) {
-    await this.closeUI();
-    await this.initUI();
+    if (!this.browser) {
+      await this.initUI();
+    } else {
+      // Just navigate to base URL and clear state
+      await this.page.goto(this.UI_BASE_URL);
+      await this.page.context().clearCookies();
+    }
   }
 
   // API test setup
@@ -35,7 +49,9 @@ Before(async function ({ pickle }) {
 After(async function ({ pickle }) {
   // UI test cleanup
   if (pickle.tags.some((tag) => tag.name === "@ui")) {
-    await this.closeUI();
+    // Clear cookies and localStorage instead of closing browser
+    await this.page.context().clearCookies();
+    await this.page.evaluate(() => window.localStorage.clear());
   }
 
   // API test cleanup
--- features/support/ui-utils.js
+++ features/support/ui-utils.js
@@ -1,5 +1,42 @@
+const { chromium } = require("@playwright/test");
+
 class UIUtils {
-  constructor() {}
+  constructor() {
+    this.UI_BASE_URL = "https://automationexercise.com";
+    this.browser = null;
+    this.context = null;
+    this.page = null;
+  }
+
+  async initBrowser() {
+    if (!this.browser) {
+      this.browser = await chromium.launch({ headless: true });
+      this.context = await this.browser.newContext();
+      this.page = await this.context.newPage();
+    }
+    return {
+      browser: this.browser,
+      context: this.context,
+      page: this.page,
+    };
+  }
+
+  async closeBrowser() {
+    if (this.browser) {
+      await this.browser.close();
+      this.browser = null;
+      this.context = null;
+      this.page = null;
+    }
+  }
+
+  getBrowserInstances() {
+    return {
+      browser: this.browser,
+      context: this.context,
+      page: this.page,
+    };
+  }
 
   capitalize(str) {
     if (!str) return "";
