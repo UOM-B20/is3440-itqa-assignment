cb96763d05a6e118c4b06099816430e339f2e08b
Dannythegr8est <155701219+praveendananjayaperera@users.noreply.github.com>
2025-01-03 20:18:04 +0530

    headless mode set to true

--- features/step_definitions/ui/verify-subscription.steps.js
+++ features/step_definitions/ui/verify-subscription.steps.js
@@ -5,7 +5,7 @@ const { chromium } = require("playwright");
 let browser, page;
 
 Given("I open the browser", async () => {
-  browser = await chromium.launch({ headless: true }); //changed to true for server
+  browser = await chromium.launch({ headless: true });
   const context = await browser.newContext();
   page = await context.newPage();
 });
